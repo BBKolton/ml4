d71380d7968dd6835844234b791cbc4979f1a249
Bryce Kolton <bbkolton@uw.edu>
2016-07-31 19:47:00 +0000

    Migrated to MySQL

--- ml4/settings.py
+++ ml4/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ml4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ml5',
+	'USER': 'root',
+	'PASSWORD': '^n1v#r$1ty0fW@ROOT',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
